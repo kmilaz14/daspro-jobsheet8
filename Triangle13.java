d76f2b01f80b38b9b8cde6d188211cf2a46129bd
kmilaz14 <kamilazhwa@gmail.com>
2024-10-31 06:10:03 +0800

    pertanyaan 2

--- Triangle13.java
+++ Triangle13.java
@@ -7,12 +7,13 @@ public class Triangle13 {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         int i = 0;
-        while (i <= N) {
+        while (i < N) {
             int j = 0;
-            while (j < i) {
+            while (j <= i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
